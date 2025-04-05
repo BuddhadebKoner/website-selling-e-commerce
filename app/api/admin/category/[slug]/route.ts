e0df41436f64b338e0d22b2093ca83ceaddee387
BuddhadebKoner <jeetkoner36@gmail.com>
2025-04-05 23:26:53 +0530

    route fixed

--- app/api/admin/category/[slug]/route.ts
+++ app/api/admin/category/[slug]/route.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
    request: NextRequest,
-   { params }: { params: { slug: string } }
+   context: { params: { slug: string } }
 ) {
    try {
       const isAdmin = isAdminRequest(request);
@@ -16,7 +16,7 @@ export async function PATCH(
          );
       }
 
-      const { slug } =  await params;
+      const { slug } = context.params;
 
       if (!slug) {
          return NextResponse.json(
